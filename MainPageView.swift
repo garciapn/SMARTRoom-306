ac6ae0b3fe926728ad19cb744a0bde8fe62c4896
garciapn <paolo.niko.garcia@gmail.com>
2017-04-25 20:12:58 -0400

    update

---
+++ MainPageView.swift
@@ -0,0 +1,20 @@
+//
+//  MainPageView.swift
+//  Final306Project
+//
+//  Created by Paolo Garcia & Alex Hansen on 1/18/17.
+//  Copyright © 2017 PNGapps. All rights reserved.
+//
+
+import Foundation
+import UIKit 
+
+class MainPageView: UIViewController {
+    
+    @IBOutlet var RoomPage: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
